0151c48eb0e20067d92340909b5c514e6561caab
smabari <mdazizulbarishafin@gmail.com>
2025-10-28 19:20:51 +0600

    01b_challenge

---
+++ pages/01b_challenge/script.js
@@ -0,0 +1,22 @@
+import gsap from "gsap";
+
+gsap.to(".card", {
+  //   x: -200,
+  //   y: -200,
+  opacity: 1,
+  rotation: 360,
+  background: "#ff6f61",
+  borderRadius: "50%",
+  //   scale: 1.25,
+  scaleX: 1.25,
+
+  //   only gsap properties
+  duration: 2,
+  //   delay: .5,
+  ease: "power1.inOut",
+  //   repeat: -1,
+  //   yoyo: true,
+  //   repeatDelay: 2,
+  paused: true,
+  stagger: 1,
+});
